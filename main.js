dc6d7971a2fba36229318f768252b8fcac5085d2
Seojisoo20191941 <amy0480@naver.com>
2023-06-24 03:46:23 +0900

    [ design ] 행맨 위치 조정

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const word = "loveborimong";
 
-const textUnderline = document.getElementById("text-field");
-textUnderline.forEach((element) => {
-  textUnderline.innerHTML += "_ ";
-});
+const l = document.querySelector("l");
+const o = document.querySelector("o");
+const v = document.querySelector("v");
+const e = document.querySelector("e");
+const b = document.querySelector("b");
+const r = document.querySelector("r");
+const i = document.querySelector("i");
+const m = document.querySelector("m");
+const n = document.querySelector("n");
+const g = document.querySelector("g");
